f83b8d85cd306cd3beb9a0c657a773298d888432
zychin2000 <zychin2000@gmail.com>
2020-03-31 02:31:11 -0700

    added spring boot

---
+++ backend/FoodMatch/src/main/java/com/zychin/FoodMatch/FoodMatchApplication.java
@@ -0,0 +1,13 @@
+package com.zychin.FoodMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMatchApplication.class, args);
+	}
+
+}
---
+++ backend/FoodMatch/src/test/java/com/zychin/FoodMatch/FoodMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zychin.FoodMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
